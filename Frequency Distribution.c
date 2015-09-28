f0b38c23cd93dae182f9fec2d539a3a754801b87
sarahtgalassi <sarahtgalassi@gmail.com>
2015-09-28 17:10:37 -0300

    versão 0.2

--- Frequency Distribution.c
+++ Frequency Distribution.c
@@ -1,26 +1,119 @@
 #include <stdio.h>
-void main()
+#include <stdlib.h>
+
+void insertion (int *inteiros, int tamanho)
 {
-    int vet[6] = {1,2,3,4,5,6};
-    int cont1, cont2 = 1, i = 1, aux;
-    cont1 = cont2;
-    while(i<6)
+		int i;
+	int j;
+	int aux;
+	for (i = 1; i < tamanho; i++){                   	// percorre da esquerda para a direita
+            aux = *(inteiros + i);                            	// separa-se o valor a ser ordenado
+            j = i - 1;
+            while ((j>=0) && (aux < *(inteiros + j))) {    	 	// percorre da direita para a esquerda até o 1º termo
+ *(inteiros + j + 1) = *(inteiros + j);                		 	//Se auxiliar é menor, os valores maiores serão copiados para a posição seguinte
+               j--;
+         }
+         if (i != j + 1) {
+*(inteiros + j + 1) = aux;                                      	//Se auxiliar é maior, ela ocupa a posição
+         }
+      }
+    }
+int dividir (int *inteiros, int esq, int dir) // esta função coloca o pivô do vetor em sua posição correta
+{
+    int aux, i = esq + 1, j = dir; // esq é o elemento tomado com pivô e dir é o último elemento do vetor
+    while(i <= j) // este ciclo percorre todo o vetor até encontrar a posição correta do pivô
     {
-        while(vet[i]>vet[i-1])
+        if(*(inteiros + i) < *(inteiros + esq) || *(inteiros + i) == *(inteiros + esq))
             i++;
-        while(vet[i] == vet[i-1])
+        if(*(inteiros + j) > *(inteiros + esq) || *(inteiros + j) == *(inteiros + esq))
+            j--;
+        if(j<i)
+            break;
+        aux = *(inteiros + j);
+        *(inteiros + j) = *(inteiros + i);
+        *(inteiros + i) = aux;
+    }
+    if (j >= 0) // esta parte coloca o pivô em sua posição correta e retorna a posição
+ {
+    aux = *(inteiros + j);
+    *(inteiros + j) = *(inteiros + esq);
+    *(inteiros + esq) = aux;
+    return j;
+}
+
+}
+void quicksort (int *inteiros, int esq, int dir) // esta função ordena um vetor de inteiros em ordem crescente
+{
+    if(esq<dir){ //esta parte checa se as posição recebidas como argumento fazem sentido
+    int pos_pivo = dividir(inteiros,esq,dir);
+    quicksort(inteiros,esq,pos_pivo -1); // recursão da função quicksort
+    quicksort(inteiros,pos_pivo +1,dir);}
+}
+
+void mais_repete(int *inteiros, int n)
+{
+    if (n > 8 && n != 1) // entre as linhas 53 e 56 é checado qual o melhor algortimo de ordenação baseado no tamanho do vetor
+    quicksort(inteiros,0, n-1);
+    if(n < 8 && n != 1)
+        insertion(inteiros, n);if (n > 8 && n != 1) // entre as linhas 53 e 56 é checado qual o melhor algortimo de ordenação baseado no tamanho do vetor
+    quicksort(inteiros,0, n-1);
+    if(n < 8 && n != 1)
+        insertion(inteiros, n);
+    int cont1 = 1, cont2, cont3 = 1, i = 1, j = 0;
+    printf("\nOs elementos que mais repetem sao:\n");
+    while(j < 1)
+    {
+
+    while(i < n)
+    {
+        while(*(inteiros + i) > *(inteiros + i - 1))
+            {
+                i++;
+            }
+            cont2 = 1;
+        while(*(inteiros + i) == *(inteiros + i - 1))
         {
             cont2++;
             i++;
         }
-        aux = vet[i-1];
-        if(cont2>cont1)
-            cont1 = cont2;
+        if (cont2 > cont3)
+            {
+                cont3 = cont2;
+            }
     }
-    if(cont1==1)
-        printf("todos sao diferentes\n");
-    if(cont1 == 6)
-        printf("todos iguais a %i\n", aux);
-    if (cont1 != 1 && cont1 != 6)
-        printf("o que mais repete eh %i\n", aux);
+
+    i = 1;
+    while(i < n)
+    {
+        while (*(inteiros + i) == *(inteiros + i - 1))
+        {
+            cont1++;
+            i++;
+        }
+        if (cont1 == cont3)
+        {
+            printf("%i\n", *(inteiros + i - 1));
+        }
+        if(*(inteiros + i) != *(inteiros + i - 1) && i < n)
+            cont1 = 1;
+        i++;
+    }
+    if(cont3 == 1)
+        printf("%i\n", *(inteiros + i - 1));
+    j = 1;
+    }
+}
+
+void main()
+{
+    int n = 0;
+    while (n <= 0){
+    printf("Digite um numero inteiro, n, positivo e maior que zero:\n");
+    scanf("%i", &n);}
+    printf("\nDigite %i numeros inteiros:\n", n);
+    int cont, *inteiros = (int *)malloc(n*sizeof(int));
+    for(cont = 0; cont < n; cont++)
+        scanf("%i", (inteiros + cont));
+    mais_repete(inteiros, n);
+    free(inteiros);
 }
