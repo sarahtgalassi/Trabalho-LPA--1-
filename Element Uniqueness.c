fbaad79c38bfcf1a3ccd25a14ada4de8a39d4be4
sarahtgalassi <sarahtgalassi@gmail.com>
2015-09-28 17:07:00 -0300

    versão 0.2

--- Element Uniqueness.c
+++ Element Uniqueness.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-int dividir (int vet[], int esq, int dir)
+void insertion (int vet[], int tamanho)
 {
-    int aux, i = esq + 1, j = dir;
-    while(i<=j)
+		int i;
+	int j;
+	int aux;
+	for (i = 1; i < tamanho; i++){                   	// percorre da esquerda para a direita
+            aux = vet[i];                            	// separa-se o valor a ser ordenado
+            j = i - 1;
+            while ((j>=0) && (aux < vet[j])) {    	 	// percorre da direita para a esquerda até o 1º termo
+ vet[j+1] = vet[j]          ;                		 	//Se auxiliar é menor, os valores maiores serão copiados para a posição seguinte
+               j--;
+         }
+         if (i != j + 1) {
+vet[j+1] = aux;                                      	//Se auxiliar é maior, ela ocupa a posição
+         }
+      }
+    }
+int dividir (int vet[], int esq, int dir) // esta função coloca o pivô do vetor em sua posição correta
+{
+    int aux, i = esq + 1, j = dir; // esq é o elemento tomado com pivô e dir é o último elemento do vetor
+    while(i <= j) // este ciclo percorre todo o vetor até encontrar a posição correta do pivô
     {
         if(vet[i]<vet[esq] || vet[i] == vet[esq])
             i++;
@@ -14,25 +31,32 @@ int dividir (int vet[], int esq, int dir)
         vet[j]=vet[i];
         vet[i]=aux;
     }
+    if (j >= 0) // esta parte coloca o pivô em sua posição correta e retorna a posição
+ {
     aux = vet[j];
     vet[j]=vet[esq];
     vet[esq]=aux;
     return j;
 }
-void quicksort (int vet[], int esq, int dir)
+
+}
+void quicksort (int vet[], int esq, int dir) // esta função ordena um vetor de inteiros em ordem crescente
 {
-    if(esq<dir){
+    if(esq<dir){ //esta parte checa se as posição recebidas como argumento fazem sentido
     int pos_pivo = dividir(vet,esq,dir);
-    quicksort(vet,esq,pos_pivo -1);
+    quicksort(vet,esq,pos_pivo -1); // recursão da função quicksort
     quicksort(vet,pos_pivo +1,dir);}
 }
 
-void unico (int vet[], int n)
+void unico (int vet[], int n) // esta função analisa quais elementos de um vetor não se repetem
 {
-   quicksort(vet,0, n-1);
-   int x = 0, y = 0;
-   int *valores_unicos = NULL;
-    while(x<n)
+   if (n > 8 && n != 1) // entre as linhas 53 e 56 é checado qual o melhor algortimo de ordenação baseado no tamanho do vetor
+    quicksort(vet,0, n-1);
+    if(n < 8 && n != 1)
+        insertion(vet, n);
+   int x = 0;
+   int *valores_unicos = NULL; // este ponteiro aponta para o endereço de memória dos valores únicos
+    while(x<=n-2) // este ciclo verifica elementos únicos entre x=0 e x=n-2
     {
         if(vet[x+1] > vet[x])
         {
@@ -44,11 +68,16 @@ void unico (int vet[], int n)
         {
             while(vet[x] == vet[x+1])
                 x++;
-            x++; //Quando o ciclo terminar vet[x] ainda ser? igual ao valor que estava repetindo,
-                 //adicionando +1 a x, o valor seguinte ser? diferente.
+            x++; //Quando o ciclo terminar vet[x] ainda será igual ao valor que estava repetindo,
+                 //adicionando +1 a x, o valor seguinte será diferente.
         }
     }
-    if(valores_unicos == NULL)
+    if (x == n-1 && vet[x] > vet[x-1]) // verifica se apenas o último valor é único
+    {
+        valores_unicos = &vet[x];
+        printf("%i\n", vet[x]);
+    }
+    if(valores_unicos == NULL && n != 1) // se o ponteiro não apontar para nenhum endereço de memória, então não há valores únicos
         printf("Nao ha valores unicos.\n");
 }
 void main()
